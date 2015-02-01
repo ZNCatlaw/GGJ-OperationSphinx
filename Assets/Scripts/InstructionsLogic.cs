a5282c6d66ab54f71ad53ae9a954748eaa0cc0a6
Tim B <nomoon@phoebus.ca>
2015-01-31 22:02:21 -0800

    Instructions screen.

---
+++ Assets/Scripts/InstructionsLogic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsLogic : MonoBehaviour
+{
+
+    GameLogic gameLogic;
+    Transform tr;
+    bool dismissed = false;
+
+	// Use this for initialization
+	void Start()
+    {
+        gameLogic = Camera.main.GetComponent<GameLogic>();
+        tr = transform;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (dismissed) {
+            if (transform.position.y < 15.0f) {
+                tr.position = tr.position + (Vector3.up * Time.deltaTime * 10.0f);
+            } else {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    void OnMouseDown()
+    {
+        dismissed = true;
+        Destroy(GameObject.Find("Instructions/Background"));
+        gameLogic.SendMessage("NewGame");
+    }
+}
